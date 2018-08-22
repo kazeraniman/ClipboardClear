d972aeaf300cec9c10ffa594d2966d8e71424666
Iman Kazerani <Kazeraniman@gmail.com>
2018-08-21 21:16:46 -0400

    Making the icon the right colour in the right places for maximum visibility.

--- ClipboardClear.Designer.cs
+++ ClipboardClear.Designer.cs
@@ -181,7 +181,6 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "ClipboardClear";
-            this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Clipboard Clear";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
